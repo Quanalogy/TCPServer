384e4ab052e374069e1d4eaa662d7eb296fbf423
Munke <aalsing@gmail.com>
2017-03-02 13:54:49 +0100

    Can now send the contents of a directory correctly from server

--- TCPServer.cpp
+++ TCPServer.cpp
@@ -41,13 +41,13 @@ int TCPServer::initServer() {
         cout << "The server has been initialized" << endl;
     }
 
-
     //SO_REUSEADDR;
     // Bind the socket, hence telling it to which address it belongs
     int error = bind(serverSocket, serverinfo->ai_addr, serverinfo->ai_addrlen);
 
     if(error == -1){
-        cout << "An error has occurred while binding the socket on server side" << endl << error << endl;
+        cout << "An error has occurred while binding the socket on server side: "
+             << endl << strerror(errno) << endl;
         return error;
     } else {
         cout << "Successfully bound the server to the socket" << endl;
@@ -127,44 +127,79 @@ size_t TCPServer::getFilesOnServer(char ***buf, const char *location,
                                    const struct sockaddr *dest_addr,
                                    socklen_t dest_len, int dest_fd) {
     DIR *dp;
-    struct dirent *ep;
-    size_t dirElements = 0;
-    dp = opendir(location);
+    struct dirent *ep;          // handler of dir
+    size_t dirElements = 0;     // Elements in dir
+    dp = opendir(location);     // Handler of current element in dir
 
-    if(dp == NULL){
+
+    if(dp == NULL){             // Is the dir open?
         cout << "Failed to open file directory" << endl;
         closedir(dp);
-    } else {
+        return 0;
+    } else {                    // It is
         *buf = NULL;
         ep = readdir(dp);
-        while(ep != NULL){
+        while(ep != NULL){      // Count elements in dir
             ++dirElements;
             ep = readdir(dp);
         }
-        rewinddir(dp);
+        rewinddir(dp);          // Start from the beginning of the dir
 
-        *buf = (char **) calloc(dirElements, sizeof(char *));
+        *buf = (char **) calloc(dirElements, sizeof(char *));   // Set size to fit the elements
 
         dirElements = 0;
         ep = readdir(dp);
-        while(ep != NULL) {
+        while(ep != NULL) {     // Set the name of each element in the dir to the buffer
             (*buf)[dirElements++] = strdup(ep->d_name);
             ep = readdir(dp);
         }
-        closedir(dp);
+        closedir(dp);           // Cleanup
+
+        // Send to the client how many elements to expect
+        char dirNum[sizeof(size_t)];
+        snprintf(dirNum, sizeof(dirNum), "%zu", dirElements);   // Convert to format that can be send
 
         ssize_t bytesSend = 0;
-        for (int i = 0; i < sizeof(*buf); i+=1000) {
-//            while(bytesSend > 0) {
-                bytesSend = sendto(dest_fd, *buf[i], 1000, 0, dest_addr, dest_len);
-                if(bytesSend == -1){
-                    cout << "Error sending list: " << strerror(errno) << endl;
-                } else {
-                    cout << "Send " << bytesSend << " bytes" << endl;
-                }
-//            }
+        bytesSend = sendto(dest_fd, &dirNum[0], dirElements, 0, dest_addr, dest_len);   // Send how many elements to expect
+
+        cout << "Dir elements to send: " << dirNum << endl;
+
+        if(bytesSend == -1) {
+            cout << "Error sending number of dir elements, quitting with error: "
+                 << strerror(errno) << endl;
+            return 0;
+        }
+
+        cout << "Elements to send:" << endl;
+        for (int j = 0; j < dirElements; ++j){
+            cout << (*buf)[j] << endl;
+        }
+
+        for (int i = 0; i < dirElements; i++) {         // Send all the elements, one by one
+            size_t sendLen = strlen((*buf)[i]);         // Length of element to send
+
+            bytesSend = sendto(dest_fd, &sendLen, sizeof(sendLen), 0, dest_addr, dest_len); // Send size of element to expect
+            if(bytesSend == -1){
+                cout << "Error sending list, quitting with error: " << strerror(errno) << endl;
+                return 0;
+            } else {
+                cout << "Send " << bytesSend << " bytes" << endl;
+            }
+
+            bytesSend = sendto(dest_fd, (*buf)[i], sendLen, 0, dest_addr, dest_len);        // Send the element
+            if(bytesSend == -1){
+                cout << "Error sending list, quitting with error: " << strerror(errno) << endl;
+                return 0;
+            } else {
+                cout << "Send " << bytesSend << " bytes" << endl;
+            }
+        }
+        // Force shutdown, hence sending the 0 byte to terminate the while on client
+        if(shutdown(dest_fd, 2) == -1) {
+            cout << "Failed closing socket, with error" << strerror(errno) << endl;
         }
 
     }
+
     return dirElements;
 }
